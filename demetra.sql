896e17cb84b1c5b76518947fa2de4656c1fbd764
gkkconan <conan.gkk@gmail.com>
2025-04-03 12:17:19 +0200

    feat(demetra.sql): add recognitions table in database schema

--- demetra.sql
+++ demetra.sql
@@ -9,14 +9,21 @@ CREATE TABLE IF NOT EXISTS sensors_data (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS recognitions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    object_name VARCHAR(255) NOT NULL,
+    object_quantity INT NOT NULL DEFAULT 1,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
 
 -- INITIAL DATA
-INSERT INTO sensors_data (id, temperature, humidity, gas, created_at) VALUES
-    (1, 25.5, 60.2, 100.0, '2025-03-23 08:00:00'),
-    (2, 22.8, 55.1, 95.5, '2025-03-23 08:00:00'),
-    (3, 28.3, 65.7, 105.2, '2025-03-23 08:00:00')
-ON DUPLICATE KEY UPDATE
-    temperature = VALUES(temperature),
-    humidity = VALUES(humidity),
-    gas = VALUES(gas),
-    created_at = VALUES(created_at);
+-- INSERT INTO sensors_data (id, temperature, humidity, gas, created_at) VALUES
+--     (1, 25.5, 60.2, 100.0, '2025-03-23 08:00:00'),
+--     (2, 22.8, 55.1, 95.5, '2025-03-23 08:00:00'),
+--     (3, 28.3, 65.7, 105.2, '2025-03-23 08:00:00')
+-- ON DUPLICATE KEY UPDATE
+--     temperature = VALUES(temperature),
+--     humidity = VALUES(humidity),
+--     gas = VALUES(gas),
+--     created_at = VALUES(created_at);
