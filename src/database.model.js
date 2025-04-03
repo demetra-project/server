273625b077b170007d36bd44d730a1f183c1574a
gkkconan <conan.gkk@gmail.com>
2025-04-03 12:19:07 +0200

    feat: update database model (add recognitions and export both schema)

---
+++ src/database.model.js
@@ -0,0 +1,24 @@
+const { sequelize } = require('./db.config');
+const { DataTypes } = require('sequelize');
+
+const SensorData = sequelize.define('sensors_data', {
+    temperature: DataTypes.DECIMAL(10, 2),
+    humidity: DataTypes.DECIMAL(10, 2),
+    gas: DataTypes.DECIMAL(10, 2),
+    created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+  }, {
+    timestamps: false,
+    underscored: true
+});
+
+const Recognitions = sequelize.define('recognitions', {
+    object: DataTypes.TEXT(),
+    quantity: DataTypes.INTEGER(),
+    created_at: { type: DataTypes.DATE, defaultValue: DataTypes.NOW }
+  }, {
+    timestamps: false,
+    underscored: true
+  }
+);
+
+module.exports = { SensorData, Recognitions };
