f4073e4fdeafb36af352408044d8ef7f59eafe55
gkkconan <conan.gkk@gmail.com>
2025-04-26 18:25:18 +0200

    feat: update db schema

--- demetra.sql
+++ demetra.sql
@@ -2,26 +2,41 @@ CREATE DATABASE IF NOT EXISTS demetra;
 USE demetra;
 
 CREATE TABLE IF NOT EXISTS sensors_data (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     temperature DECIMAL(10, 2) NOT NULL,
     humidity DECIMAL(10, 2) NOT NULL,
     gas DECIMAL(10, 2) NOT NULL,
     gps_lat DECIMAL(10, 7) NOT NULL,
     gps_lon DECIMAL(10, 7) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (gps_lat, gps_lon, created_at)
 );
 
 CREATE TABLE IF NOT EXISTS recognitions (
     id INT AUTO_INCREMENT PRIMARY KEY,
     object_name VARCHAR(255) NOT NULL,
     object_quantity INT NOT NULL DEFAULT 1,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    gps_lat DECIMAL(10, 7) NOT NULL,
+    gps_lon DECIMAL(10, 7) NOT NULL,
+    sensor_created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (gps_lat, gps_lon, sensor_created_at)
+        REFERENCES sensors_data(gps_lat, gps_lon, created_at)
+        ON DELETE CASCADE
 );
 
+-- CREATE TABLE IF NOT EXISTS places (
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     lat DECIMAL(10, 7) NOT NULL,
+--     lon DECIMAL(10, 7) NOT NULL,
+--     name VARCHAR(255) NOT NULL,
+--     objects_id INT NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
 
-CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
+
+-- CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
+-- GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+-- FLUSH PRIVILEGES;
 
 
 -- INITIAL DATA
